704ca2c23bd96d5a084086e27ba128fccde3cd8e
michael <mikek222@gmail.com>
2023-11-28 19:57:44 -0500

    added local storage functionality

--- assets/script.js
+++ assets/script.js
@@ -55,28 +55,31 @@ function geoLocate(city) {
             let fiveDayHumidities = [];
             let fiveDayWindSpeeds = [];
             let fiveDayIcons = [];
+            let fiveDayDates = [];
 
             console.log(fiveDay);
 
-            for (let i = 0; i < 5; i++) {
+            for (let i = 0; i < 6; i++) {
                 const forecast = fiveDay.list[i];
                 const temperature = forecast.main.temp;
                 const humidity = forecast.main.humidity
                 const windSpeed = forecast.wind.speed
                 const icons = "https://openweathermap.org/img/wn/" + forecast.weather[0].icon + "@2x.png"
-                // const date = GONNA NEED DAY.JS FOR THIS
+                const date = dayjs(forecast.dt_txt).format('MMMM D, YYYY');
 
                 fiveDayTemps.push(temperature);
                 fiveDayHumidities.push(humidity);
                 fiveDayWindSpeeds.push(windSpeed);
                 fiveDayIcons.push(icons)
+                fiveDayDates.push(date)
 
                 console.log(fiveDayTemps);
                 console.log(fiveDayHumidities);
                 console.log(fiveDayWindSpeeds);
-                console.log(fiveDayIcons)
+                console.log(fiveDayIcons);
+                console.log(fiveDayDates)
 
-                displayFiveDay(fiveDayHumidities, fiveDayWindSpeeds, fiveDayIcons, fiveDayTemps)
+                displayFiveDay(fiveDayHumidities, fiveDayWindSpeeds, fiveDayIcons, fiveDayTemps, fiveDayDates)
             }
         })
 
@@ -89,11 +92,12 @@ function geoLocate(city) {
 
 }
 // Below is a Proof of concept for displaying info for the 5 day.
-function displayWeatherInfo(temp, feelsLike, humidity, cityName, icon, windSpeed) {
+function displayWeatherInfo(temp, feelsLike, humidity, cityName, icon, windSpeed,) {
     const weatherContainer = document.querySelector(".card-body");
+    var today = dayjs().format('MMM D, YYYY');
 
     weatherContainer.innerHTML = `
-        <h4 class="mb-1 sfw-normal">${cityName}</h4>
+        <h4 class="mb-1 sfw-normal">${cityName}<br>${(today)}</h4>
         <p class="mb-2">Current temperature: <strong>${temp} °F</strong></p>
         <p>Feels like: <strong>${feelsLike} °F</strong></p>
         <p>Humidity: <strong>${humidity}%</strong></p>
@@ -107,37 +111,42 @@ function displayWeatherInfo(temp, feelsLike, humidity, cityName, icon, windSpeed
 }
 
 
-function displayFiveDay(fiveDayHumidities, fiveDayWindSpeeds, fiveDayIcons, fiveDayTemps) {
+function displayFiveDay(fiveDayHumidities, fiveDayWindSpeeds, fiveDayIcons, fiveDayTemps, fiveDayDates) {
     const fiveDayConatiner = document.querySelector(".py-5")
 
     fiveDayConatiner.innerHTML = `
     <div class="d-flex justify-content-between">
     <div class="forecast-container">
       <div class="forecast-box">
+      <p> <strong> ${fiveDayDates[1]} </strong> </p>
       <p>Temp: <strong> ${fiveDayTemps[0]}</strong> </p>
       <p>Humidity: <strong>${fiveDayHumidities[0]}%</strong></p>
       <p>Wind Speed: <strong>${fiveDayWindSpeeds[0]} MPH</strong> </p>
       <p><img src=${fiveDayIcons[0]}> </p>
       </div>
       <div class="forecast-box">
+      <p> <strong> ${fiveDayDates[2]} </strong> </p>
       <p>Temp: <strong>${fiveDayTemps[1]}</strong> </p>
       <p>Humidity:<strong> ${fiveDayHumidities[1]}%</strong></p>
       <p>Wind Speed:<strong> ${fiveDayWindSpeeds[1]} MPH</strong> </p>
       <p> <img src=${fiveDayIcons[1]}> </p>
       </div>
       <div class="forecast-box">
+      <p> <strong> ${fiveDayDates[3]} </strong> </p>
       <p>Temp:<strong> ${fiveDayTemps[2]}</strong> </p>
       <p>Humidity:<strong> ${fiveDayHumidities[2]}%</strong></p>
       <p>Wind Speed:<strong> ${fiveDayWindSpeeds[2]} MPH </strong></p>
       <p> <img src=${fiveDayIcons[2]}> </p>
       </div>
       <div class="forecast-box">
+      <p> <strong> ${fiveDayDates[4]} </strong> </p>
       <p>Temp:<strong> ${fiveDayTemps[3]}</strong> </p>
       <p>Humidity:<strong> ${fiveDayHumidities[3]}%</strong></p>
       <p>Wind Speed:<strong> ${fiveDayWindSpeeds[3]} MPH </strong></p>
       <p><img src=${fiveDayIcons[3]}> </p>
       </div>
       <div class="forecast-box">
+      <p> <strong> ${fiveDayDates[5]} </strong> </p>
       <p>Temp:<strong> ${fiveDayTemps[4]} </strong></p>
       <p>Humidity:<strong> ${fiveDayHumidities[4]}%</strong></p>
       <p>Wind Speed:<strong> ${fiveDayWindSpeeds[4]} MPH</strong> </p>
@@ -147,6 +156,10 @@ function displayFiveDay(fiveDayHumidities, fiveDayWindSpeeds, fiveDayIcons, five
   </div>`
 }
 
+function saveUserCitiesToLocalStorage(cities) {
+    localStorage.setItem('userCities', JSON.stringify(cities));
+}
+
 
 // Display Error function for inner HTML
 // function displayError(message) {
@@ -154,11 +167,15 @@ function displayFiveDay(fiveDayHumidities, fiveDayWindSpeeds, fiveDayIcons, five
 // }
 
 formEl.addEventListener("submit", function (event) {
-    event.preventDefault()
-    const userCity = getUserCity.value
-    console.log(userCity)
-    geoLocate(userCity)
+    event.preventDefault();
+    const userCity = getUserCity.value;
+    const existingUserCities = JSON.parse(localStorage.getItem('userCities')) || [];
+    existingUserCities.push(userCity);
+    saveUserCitiesToLocalStorage(existingUserCities);
+    geoLocate(userCity);
+    
 
 });
+// Will need to iterate and compare existing storage and not allow a copy of the storage when creating the buttons for local storage
 
 // could use something to add a .notation 5 day weather forcast to the existing variable that will hold temp, humidity, feels like, ect //
